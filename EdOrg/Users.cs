26cf9f9f828c925b1ec6d864bf0c00ae1d88ad39
wombat1742 <tomgogard@gmail.com>
2024-10-10 22:02:29 +0300

    1

--- EdOrg/Users.cs
+++ EdOrg/Users.cs
@@ -11,7 +11,7 @@ namespace EdOrg
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Users
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,7 +20,7 @@ namespace EdOrg
             this.Marks = new HashSet<Marks>();
             this.Subjects = new HashSet<Subjects>();
         }
-    
+
         public int Id { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
